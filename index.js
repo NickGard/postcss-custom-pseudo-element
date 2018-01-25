b86c1a5eaec0741a9755f8369cf811819a093262
Nick Gard <nick.gard@nike.com>
2018-01-25 14:27:03 -0800

    Fix lint errors

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-custom-pseudo-element', () => {
-  const PSEUDO_PREFIX = '::-cpe-';
-  const pseudoRE = RegExp(`${PSEUDO_PREFIX}([a-zA-Z\-]+)`);
+    const PSEUDO_PREFIX = '::-cpe-';
+    const pseudoRE = RegExp(`${PSEUDO_PREFIX}([a-zA-Z\-]+)`);
 
-  const resolvePseudoSelector = (selector, warn) => {
-    if (selector.indexOf(PSEUDO_PREFIX) > -1) {
-      try {
-        const [pseudoSelector, pseudoName] = pseudoRE.exec(selector);
-        return selector.replace(pseudoSelector, ` [data-pseudo="${pseudoName}"]`);
-      } catch (e) {
-        warn(`Could not resolve selector: ${selector}\n${e}`);
-      }
-    }
-    return selector;
-  };
+    const resolvePseudoSelector = (selector, warn) => {
+        if (selector.indexOf(PSEUDO_PREFIX) > -1) {
+            try {
+                const [pseudoSelector, pseudoName] = pseudoRE.exec(selector);
+                return selector.replace(
+                    pseudoSelector,
+                    ` [data-pseudo="${pseudoName}"]`
+                );
+            } catch (e) {
+                warn(`Could not resolve selector: ${selector}\n${e}`);
+            }
+        }
+        return selector;
+    };
 
-  return (root, result) => {
-    // Replace pseudo selectors with their aliases
-    root.walkRules(rule => {
-      rule.selector = resolvePseudoSelector(rule.selector, err => rule.warn(result, err));
-    });
-  };
+    return (root, result) => {
+        root.walkRules(rule => {
+            rule.selector = resolvePseudoSelector(rule.selector, result.warn);
+        });
+    };
 });
